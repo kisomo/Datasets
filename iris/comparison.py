db5fba4bd5497806c11b1f2d8214d986323a2606
kisomo <tmuthoka@gmail.com>
2018-06-09 22:23:38 -0500

    added catboost

--- iris/comparison.py
+++ iris/comparison.py
@@ -37,6 +37,8 @@ print(pd.DataFrame(X_train, columns=X_df.columns).head())
 # Print the unique labels of the dataset
 print('\n' + 'The unique labels in this data are ' + str(np.unique(y)))
 
+
+
 #+++++++++++++++++++++++++++++++++++++++ Visualize ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #https://www.kaggle.com/mgabrielkerr/visualizing-knn-svm-and-xgboost-on-iris-dataset
 
@@ -83,20 +85,30 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
 
 
 
-
-
-
-
-
-
 #+++++++++++++++++++++++++++++++++++++++++++++++ AdaBoost +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+#https://www.ritchieng.com/machine-learning-ensemble-of-learners-adaboost/
 
+from sklearn.metrics import accuracy_score
+from sklearn.cross_validation import train_test_split
+from sklearn.datasets import load_iris
+from sklearn.ensemble import AdaBoostClassifier
+#Instantiate
+abc = AdaBoostClassifier()
 
+# Fit
+abc.fit(X_train, y_train)
 
+# Predict
+y_pred = abc.predict(X_test)
+
+# Accuracy
+AdaBoost_score = accuracy_score(y_pred, y_test)
+print("AdaBoost score = {:.4f}" .format(AdaBoost_score))
 
 
 #+++++++++++++++++++++++++++++++++++++++++++ xgboost ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 #https://www.kdnuggets.com/2017/03/simple-xgboost-tutorial-iris-dataset.html
 
 import xgboost as xgb
@@ -123,7 +135,7 @@ bst = xgb.train(param, dtrain, num_round)
 bst.dump_model('dump.raw.txt')
 
 preds = bst.predict(dtest)
-print(preds)
+#print(preds)
 
 import numpy as np
 best_preds = np.asarray([np.argmax(line) for line in preds])
@@ -140,6 +152,7 @@ joblib.dump(bst, 'bst_model.pkl', compress=True)
 
 
 #++++++++++++++++++++++++++++++++++++++++++ xgboost ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 #https://www.kaggle.com/mgabrielkerr/visualizing-knn-svm-and-xgboost-on-iris-dataset
 
 import xgboost as xgb
@@ -156,11 +169,235 @@ print('XGB_CLF score = {:.4f}' .format(xgb_clf_score))
 #plot_decision_regions(X_test_std, y_test, xgb_clf)
 
 
+
 #++++++++++++++++++++++++++++++++++++++++++++++ lightgbm ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+#https://github.com/Microsoft/LightGBM/blob/master/examples/python-guide/advanced_example.py
+
+import json
+import lightgbm as lgb
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error
+
+try:
+    import cPickle as pickle
+except BaseException:
+    import pickle
 
 
+num_train, num_feature = X_train.shape
+#print(num_train)
+#print(num_feature)
 
+
+# create dataset for lightgbm
+# if you want to re-use data, remember to set free_raw_data=False
+lgb_train = lgb.Dataset(X_train, y_train, free_raw_data=False)
+lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train, free_raw_data=False)
+
+
+# specify your configurations as a dict
+params = {
+    'boosting_type': 'gbdt',
+    'objective': 'binary',
+    'metric': 'binary_logloss',
+    'num_leaves': 31,
+    'learning_rate': 0.05,
+    'feature_fraction': 0.9,
+    'bagging_fraction': 0.8,
+    'bagging_freq': 5,
+    'verbose': 0
+}
+
+# generate a feature name
+feature_name = ['feature_' + str(col) for col in range(num_feature)]
+
+print('Start training...')
+# feature_name and categorical_feature
+gbm = lgb.train(params,
+                lgb_train,
+                num_boost_round=10,
+                valid_sets=lgb_train,  # eval training data
+                feature_name=feature_name,
+                categorical_feature=[21])
+
+# check feature name
+print('Finish first 10 rounds...')
+print('4th feature name is:', repr(lgb_train.feature_name[3]))
+
+
+# save model to file
+gbm.save_model('model.txt')
+
+# dump model to json (and save to file)
+print('Dump model to JSON...')
+model_json = gbm.dump_model()
+
+with open('model.json', 'w+') as f:
+    json.dump(model_json, f, indent=4)
+
+# feature names
+print('Feature names:', gbm.feature_name())
+
+# feature importances
+print('Feature importances:', list(gbm.feature_importance()))
+
+# load model to predict
+print('Load model to predict')
+bst = lgb.Booster(model_file='model.txt')
+# can only predict with the best iteration (or the saving iteration)
+y_pred = bst.predict(X_test)
+# eval with loaded model
+print('The rmse of loaded model\'s prediction is:', mean_squared_error(y_test, y_pred) ** 0.5)
+
+
+# dump model with pickle
+with open('model.pkl', 'wb') as fout:
+    pickle.dump(gbm, fout)
+# load model with pickle to predict
+with open('model.pkl', 'rb') as fin:
+    pkl_bst = pickle.load(fin)
+# can predict with any iteration when loaded in pickle way
+y_pred = pkl_bst.predict(X_test, num_iteration=7)
+# eval with loaded model
+print('The rmse of pickled model\'s prediction is:', mean_squared_error(y_test, y_pred) ** 0.5)
+
+
+# continue training
+# init_model accepts:
+# 1. model file name
+# 2. Booster()
+gbm = lgb.train(params,
+                lgb_train,
+                num_boost_round=10,
+                init_model='model.txt',
+                valid_sets=lgb_eval)
+
+print('Finish 10 - 20 rounds with model file...')
+
+# decay learning rates
+# learning_rates accepts:
+# 1. list/tuple with length = num_boost_round
+# 2. function(curr_iter)
+gbm = lgb.train(params,
+                lgb_train,
+                num_boost_round=10,
+                init_model=gbm,
+                learning_rates=lambda iter: 0.05 * (0.99 ** iter),
+                valid_sets=lgb_eval)
+
+print('Finish 20 - 30 rounds with decay learning rates...')
+
+# change other parameters during training
+gbm = lgb.train(params,
+                lgb_train,
+                num_boost_round=10,
+                init_model=gbm,
+                valid_sets=lgb_eval,
+                callbacks=[lgb.reset_parameter(bagging_fraction=[0.7] * 5 + [0.6] * 5)])
+
+print('Finish 30 - 40 rounds with changing bagging_fraction...')
+
+# self-defined objective function
+# f(preds: array, train_data: Dataset) -> grad: array, hess: array
+# log likelihood loss
+def loglikelood(preds, train_data):
+    labels = train_data.get_label()
+    preds = 1. / (1. + np.exp(-preds))
+    grad = preds - labels
+    hess = preds * (1. - preds)
+    return grad, hess
+
+
+# self-defined eval metric
+# f(preds: array, train_data: Dataset) -> name: string, value: array, is_higher_better: bool
+# binary error
+def binary_error(preds, train_data):
+    labels = train_data.get_label()
+    return 'error', np.mean(labels != (preds > 0.5)), False
+
+
+gbm = lgb.train(params,
+                lgb_train,
+                num_boost_round=10,
+                init_model=gbm,
+                fobj=loglikelood,
+                feval=binary_error,
+                valid_sets=lgb_eval)
+
+print('Finish 40 - 50 rounds with self-defined objective function and eval metric...')
+
+print('Start a new training job...')
+
+# callback
+def reset_metrics():
+    def callback(env):
+        lgb_eval_new = lgb.Dataset(X_test, y_test, reference=lgb_train)
+        if env.iteration - env.begin_iteration == 5:
+            print('Add a new valid dataset at iteration 5...')
+            env.model.add_valid(lgb_eval_new, 'new valid')
+    callback.before_iteration = True
+    callback.order = 0
+    return callback
+
+
+gbm = lgb.train(params,
+                lgb_train,
+                num_boost_round=10,
+                valid_sets=lgb_train,
+                callbacks=[reset_metrics()])
+
+print('Finish first 10 rounds with callback function...')
+
+#++++++++++++++++++++++++++++++++++++++++++++++++ lightgbm ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#https://github.com/Microsoft/LightGBM/issues/991
+
+from sklearn import datasets
+import numpy as np
+import lightgbm as lgb
+param_algo = {
+                'task': 'train',
+                'boosting_type': 'gbdt',
+                'objective': 'multiclass',
+                'num_class' : 3,
+                'metric':{'multi_logloss'},
+                'num_leaves': 31,
+                'min_data_in_leaf':1,
+                'learning_rate':0.1,
+                'zero_as_missing':False,
+            }
+
+feature_name=['a', 'b', 'c', 'd']
+
+lgb_train = lgb.Dataset(X_train, 
+                        y_train,
+                        feature_name=feature_name) #,categorical_feature=[0])
+
+gbm = lgb.train(param_algo,
+                lgb_train, 
+                num_boost_round=6, 
+                valid_sets=[lgb_train],
+                feature_name=feature_name) #,categorical_feature=[0])
+
+
+res = gbm.predict(X_test)
+#print(res)
+
+best_predss = np.asarray([np.argmax(line) for line in res])
+
+#print(best_predss)
+#print(y_test)
+
+correct = [estimate== target for estimate, target in zip(best_predss, y_test)]
+
+#print(correct)
+
+accuracy =  sum(correct) / len(correct)
+
+#lgbm_score = gbm.score(y_test, best_preds, average='macro')
+print("LightGBM score = {:.4f} " .format(accuracy))
 
 
 
@@ -228,6 +465,7 @@ print('k_NN score = {:.4f}' .format(kNN_score))
 
 
 #+++++++++++++++++++++++++++++++++++++++++++++++ svm +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 #https://www.kaggle.com/mgabrielkerr/visualizing-knn-svm-and-xgboost-on-iris-dataset
 
 from sklearn.svm import SVC
@@ -273,6 +511,62 @@ print('SVM score = {:.4f}' .format(svm_score))
 
 
 
+'''
+print("++++++++++++++++++++++++++++++++++++++++ SOM +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
-#++++++++++++++++++++++++++++++++++++++++ SOM +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#https://dzone.com/articles/self-organizing-maps
+
+from numpy import genfromtxt,array,linalg,zeros,apply_along_axis
+
+# reading the iris dataset in the csv format    
+# (downloaded from http://aima.cs.berkeley.edu/data/iris.csv)
+#data = genfromtxt('iris.csv', delimiter=',',usecols=(0,1,2,3))
+# normalization to unity of each pattern in the data
+#data = apply_along_axis(lambda x: x/linalg.norm(x),1,data)
+data = X_train
+from minisom import MiniSom
+### Initialization and training ###
+som = MiniSom(7,7,4,sigma=1.0,learning_rate=0.5)
+som.random_weights_init(data)
+print("Training...")
+som.train_random(data,100) # training with 100 iterations
+print("\n...ready!")
+
+
+from pylab import plot,axis,show,pcolor,colorbar,bone
+bone()
+pcolor(som.distance_map().T) # distance map as background
+colorbar()
+# loading the labels
+#target = genfromtxt('iris.csv', delimiter=',',usecols=(4),dtype=str)
+target = y_train
+
+t = zeros(len(target),dtype=int)
+print(t)
+t[target == 'setosa'] = 0
+t[target == 'versicolor'] = 1
+t[target == 'virginica'] = 2
+print(t)
+
+
+# use different colors and markers for each label
+markers = ['o','s','D']
+colors = ['r','g','b']
+for cnt,xx in enumerate(data):
+ w = som.winner(xx) # getting the winner
+ # palce a marker on the winning position for the sample xx
+ plot(w[0]+.5,w[1]+.5,markers[t[cnt]],markerfacecolor='None',
+   markeredgecolor=colors[t[cnt]],markersize=12,markeredgewidth=2)
+axis([0,som.weights.shape[0],0,som.weights.shape[1]])
+show() # show the figure
+'''
+
+
+
+
+
+
+
+
+
 
